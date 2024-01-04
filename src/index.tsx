a06d486bb22becdb8790520d7adc7a4e6f90a20e
Mushfiq Mahmud <mumahmud@achieve.com>
2024-01-04 17:39:01 -0500

    chore(fonts): add all font weights from SF Pro Display

--- src/index.tsx
+++ src/index.tsx
@@ -12,16 +12,27 @@ import JobExperience from "./components/JobExperience";
 import Objective from "./components/Objective";
 import Projects from "./components/Projects";
 
-// TODO: add all font weights
 const fontPath = "./fonts/sf-pro-display";
 Font.register({
   family: "SF Pro Display", fonts: [
-    { src: `${fontPath}/SF-Pro-Display-Light.otf`, fontStyle: "normal", fontWeight: 200 },
-    { src: `${fontPath}/SF-Pro-Display-LightItalic.otf`, fontStyle: "italic", fontWeight: 200 },
-    { src: `${fontPath}/SF-Pro-Display-Regular.otf`, fontStyle: "normal", fontWeight: 400 },
-    { src: `${fontPath}/SF-Pro-Display-RegularItalic.otf`, fontStyle: "italic", fontWeight: 400 },
-    { src: `${fontPath}/SF-Pro-Display-Black.otf`, fontStyle: "normal", fontWeight: 900 },
-    { src: `${fontPath}/SF-Pro-Display-BlackItalic.otf`, fontStyle: "italic", fontWeight: 900 },
+    { src: `${fontPath}/SF-Pro-Display-Thin.otf`, fontStyle: "normal", fontWeight: "thin" },
+    { src: `${fontPath}/SF-Pro-Display-ThinItalic.otf`, fontStyle: "italic", fontWeight: "thin" },
+    { src: `${fontPath}/SF-Pro-Display-Ultralight.otf`, fontStyle: "normal", fontWeight: "ultralight" },
+    { src: `${fontPath}/SF-Pro-Display-UltralightItalic.otf`, fontStyle: "italic", fontWeight: "ultralight" },
+    { src: `${fontPath}/SF-Pro-Display-Light.otf`, fontStyle: "normal", fontWeight: "light" },
+    { src: `${fontPath}/SF-Pro-Display-LightItalic.otf`, fontStyle: "italic", fontWeight: "light" },
+    { src: `${fontPath}/SF-Pro-Display-Regular.otf`, fontStyle: "normal", fontWeight: "normal" },
+    { src: `${fontPath}/SF-Pro-Display-RegularItalic.otf`, fontStyle: "italic", fontWeight: "normal" },
+    { src: `${fontPath}/SF-Pro-Display-Medium.otf`, fontStyle: "normal", fontWeight: "medium" },
+    { src: `${fontPath}/SF-Pro-Display-MediumItalic.otf`, fontStyle: "italic", fontWeight: "medium" },
+    { src: `${fontPath}/SF-Pro-Display-Semibold.otf`, fontStyle: "normal", fontWeight: "semibold" },
+    { src: `${fontPath}/SF-Pro-Display-SemiboldItalic.otf`, fontStyle: "italic", fontWeight: "semibold" },
+    { src: `${fontPath}/SF-Pro-Display-Bold.otf`, fontStyle: "normal", fontWeight: "bold" },
+    { src: `${fontPath}/SF-Pro-Display-BoldItalic.otf`, fontStyle: "italic", fontWeight: "bold" },
+    { src: `${fontPath}/SF-Pro-Display-Black.otf`, fontStyle: "normal", fontWeight: "ultrabold" },
+    { src: `${fontPath}/SF-Pro-Display-BlackItalic.otf`, fontStyle: "italic", fontWeight: "ultrabold" },
+    { src: `${fontPath}/SF-Pro-Display-Heavy.otf`, fontStyle: "normal", fontWeight: "heavy" },
+    { src: `${fontPath}/SF-Pro-Display-HeavyItalic.otf`, fontStyle: "italic", fontWeight: "heavy" },
   ]
 });
 
